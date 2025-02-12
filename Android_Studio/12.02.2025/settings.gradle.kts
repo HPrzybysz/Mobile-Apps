3ba9857ea0598ea1951d435b4c087d1e1192930b
HPrzybysz <niewica@gmail.com>
2025-02-12 08:32:27 +0100

    started welcome app

---
+++ Android_Studio/12.02.2025/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "12.02.2k25"
+include(":app")
+ 
